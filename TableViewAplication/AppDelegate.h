b7f8e2f688b18132df01ccae3df941313ffef607
Héctor Omar Silva Díaz <Hache@Hectors-Mac-2.local>
2014-05-23 17:47:04 -0500

    Initial Commit

---
+++ TableViewAplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewAplication
+//
+//  Created by Héctor Omar Silva Díaz on 5/23/14.
+//  Copyright (c) 2014 Hache Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewAplication/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewAplication
+//
+//  Created by Héctor Omar Silva Díaz on 5/23/14.
+//  Copyright (c) 2014 Hache Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
